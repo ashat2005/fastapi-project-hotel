eb0b86c77803e7ff0e19a3eee98235ba02c91abf
ashat2005 <ashatkydyrov61@gmail.com>
2024-02-03 05:57:41 -0500

    f-1-4-7

--- app/main.py
+++ app/main.py
@@ -6,24 +6,24 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class SHotel(BaseModel):
-    address : str
-    name : str
-    start : int 
+    address: str
+    name: str
+    stars: int 
 
 @app.get("/hotels", response_model=list[SHotel])
 def get_hotels( 
     location : str,
     date_from : date, 
     date_to : date,
-    stars : Optional[int] = Query(None, ge=1, le=5),
     has_spa : Optional[bool] = None,
-):
+    stars : Optional[int] = Query(None, ge=1, le=5),
+) -> list[SHotel]:
     hotels = [
         {
-            "address" :  "ул. Ленина, 255 , Ош",
-            "name" : "Super Hotel",
-            "stars" : 5,
-        }
+            "address": "ул. Ленина, 255, Ош",
+            "name": "Super Hotel",
+            "stars": 5,
+        },
     ]
     return  hotels
 
