31e106de378f6ce333ff89fe276db3fe5bb18e70
ashat2005 <ashatkydyrov61@gmail.com>
2024-02-03 04:59:53 -0500

    f-1-4-7

--- app/main.py
+++ app/main.py
@@ -5,8 +5,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class SHotel(BaseModel):
+    address : str
+    name : str
+    start : int 
 
-@app.get("/hotels")
+@app.get("/hotels", response_model=list[SHotel])
 def get_hotels( 
     location : str,
     date_from : date, 
@@ -14,13 +18,20 @@ def get_hotels(
     stars : Optional[int] = Query(None, ge=1, le=5),
     has_spa : Optional[bool] = None,
 ):
-    return  date_from, date_to
+    hotels = [
+        {
+            "address" :  "ул. Ленина, 255 , Ош",
+            "name" : "Super Hotel",
+            "stars" : 5,
+        }
+    ]
+    return  hotels
 
 class SBooking(BaseModel):
     room_id : int
     date_from : date 
     date_to : date
-    
+
 @app.post("/bookings")
 def add_booming(booking: SBooking):
     pass
