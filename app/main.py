874a18645a6c27d36f6f6963c62c6e7e269773ca
ashat2005 <ashatkydyrov61@gmail.com>
2024-02-03 03:52:46 -0500

    fastapi project, hotel

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Query
+from datetime import date
+from typing import Optional
+
+
+app = FastAPI()
+
+
+@app.get("/hotels")
+def get_hotels( 
+    location : str,
+    date_from : date, 
+    date_to : date,
+    stars : Optional[int] = Query(None, ge=1, le=5),
+    has_spa : Optional[bool] = None,
+):
+    return  date_from, date_to
